24df91dfe5beff6c76cc84e644b46df7438c0e16
Dustin Schnee <schnee72@gmail.com>
2017-09-13 20:56:17 -0500

    testing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-console */
 const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const compression = require('compression');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 1337;
+
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(compression());
+
+if (process.env.NODE_ENV === 'production')
+  app.use(morgan('combined'));
+else
+  app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -16,5 +31,5 @@ app.listen(port, err => {
   if (err)
     console.log(err);
   else
-    console.log(`listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
