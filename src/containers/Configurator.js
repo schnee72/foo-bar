42edeae3a353faf44b6e0f1ab3503176bbbb1dda
Dustin Schnee <schnee72@gmail.com>
2017-09-24 21:32:46 -0500

    configurator

--- src/containers/Configurator.js
+++ src/containers/Configurator.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Configurator = () =>
   <div>
     <h1>Configurator</h1>
+    <select>
+      <option>Text</option>
+      <option>Input</option>
+      <option>Link</option>
+    </select>
+    <button>+</button>
   </div>;
 
 export default Configurator;
