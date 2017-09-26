482d5e0bdd9e08c55090d1bcf1eb06850c18700b
Dustin Schnee <schnee72@gmail.com>
2017-09-25 19:15:31 -0500

    config

--- src/containers/Configurator.js
+++ src/containers/Configurator.js
@@ -3,10 +3,20 @@ import React from 'react';
 const Configurator = () =>
   <div>
     <h1>Configurator</h1>
+    <p>
+      <label>Title</label>
+      <input />
+    </p>
+    <p><button>Add Page</button></p>
     <select>
+      <option>Header</option>
+      <option>Content</option>
       <option>Text</option>
+      <option>Image</option>
       <option>Input</option>
       <option>Link</option>
+      <option>Nav</option>
+      <option>Footer</option>
     </select>
     <button>+</button>
   </div>;
